dfaad47cf5273cc21c9f671bd1e71c8917bcd858
eliteabhi <abhirangarajan2003@gmail.com>
2024-02-25 08:39:33 -0600

    sqlite in requirements.txt

--- src/example.py
+++ src/example.py
@@ -1,6 +1,6 @@
 
 from ariadne.asgi import GraphQL
-from ariadne import gql, QueryType, make_executable_schema
+from ariadne import MutationType, gql, QueryType, make_executable_schema
 from ariadne.explorer import ExplorerGraphiQL
 
 # Define type definitions (schema) using SDL
@@ -10,18 +10,27 @@ type_defs = gql(
        places: [Place]
    }
 
+   type Mutation {
+       edit_place(name: String!): Response
+   }
 
    type Place {
        name: String!
        description: String!
        country: String!
-       }  
+       }
+    
+    type Response {
+        content: String!
+    }
    """
 )
 
 # Initialize query
 
 query = QueryType()
+
+mutation = MutationType()
 
 # Define resolvers
 @query.field("places")
@@ -34,11 +43,15 @@ def places(*_):
            "description": "The city of big buildings",
            "country": "United Kingdom",
        },
-       {"name": "asdfasdfasdf", "description": "The city of pizza"}
+       {"name": "asdfasdfasdf", "description": "The city of pizza", "country": "Italy"}
    ]
 
+@mutation.field("edit_place")
+def edit_name(obj, infom, name):
+    return {"content": name}
+
 # Create executable schema
-schema = make_executable_schema(type_defs, query)
+schema = make_executable_schema(type_defs, query, mutation)
 
 # Create ASGI application
 app = GraphQL(schema)
